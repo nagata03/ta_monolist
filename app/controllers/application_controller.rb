402d1d854949766c1d2d6b49786ff745d83a58ca
s.nagata <nagata_sho@kakaku.com>
2018-10-03 06:34:41 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  include SessionsHelper
+  
+  private
+  
+  def require_user_logged_in  # 複数のControllerで使いたいメソッドなので、ApplicationControllerに書く
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+  
 end
