eaac678b57c304f1a04234b0a37a65a32680112b
s.nagata <nagata_sho@kakaku.com>
2018-10-04 06:55:47 +0000

    ranking

--- app/models/want.rb
+++ app/models/want.rb
@@ -1,3 +1,9 @@
 class Want < Ownership
   # レコードの保存はownershipsテーブル
+  
+  # クラスメソッド（インスタンスからは呼び出せない）
+  def self.ranking
+    self.group(:item_id).order('count_item_id DESC').limit(10).count(:item_id)  # count_item_idは一時的なカウント用カラム
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
+  get 'rankings/want', to: 'rankings#want'
+  
   resources :items, only: [:show, :new]
   resources :ownerships, only: [:create, :destroy]
 end
